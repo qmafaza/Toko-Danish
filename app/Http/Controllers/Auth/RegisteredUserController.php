338eaeb761b533e085ae630da08ca4f72d0d9cf7
qmafaza <qmafaza@student.uns.ac.id>
2025-04-24 13:41:11 +0700

    add cart and shop icon

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cart;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -39,6 +40,10 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+        ]);
+
+        Cart::create([
+            'user_id' => $user->id
         ]);
 
         event(new Registered($user));
