3d1753fbaaf3f8a29e725a6160e49b3469a6acf5
qmafaza <qmafaza@student.uns.ac.id>
2025-04-24 16:06:24 +0700

    bibib

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+         
+        $middleware->alias([
+
+            'seller' => \App\Http\Middleware\Seller::class,
+            
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
