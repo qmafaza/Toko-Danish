935226550679da3a99ccd393ed0a9b164ae5adce
qmafaza <qmafaza@student.uns.ac.id>
2025-04-25 13:42:31 +0700

    change dashboard to default view

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
+// ->middleware(['auth', 'verified'])
 
 Route::get('/product', function () {
     return view('product.show');
@@ -24,8 +25,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
     Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+
     Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
+    
     Route::get('/seller/setup', [SellerController::class, 'index'])->name('seller.registerseller');
 });
 
