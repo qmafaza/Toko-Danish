81253e6733fe998926112baee95e3a748660a9ea
Abqli484 <abidawaqli@gmail.com>
2025-04-26 16:10:05 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -30,18 +30,11 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
 
-<<<<<<< HEAD
-    Route::get('/seller/setup', [SellerController::class, 'setup'])->name('seller.registerseller');
-    Route::get('/seller/index', [SellerController::class, 'index'])->name('seller.sellerprofile');
-    
-    Route::post('/seller', [SellerController::class, 'store'])->name('seller.store');
-=======
     Route::get('/seller/setup', [SellerController::class, 'setup'])->name('seller.register');
 
     Route::middleware('seller')->group(function () {
         Route::get('/seller/index', [SellerController::class, 'index'])->name('seller.profile');
     });
->>>>>>> 7c23b693fd7b04c3fb432d1c408122b424b1e756
 });
 
 require __DIR__.'/auth.php';
