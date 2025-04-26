49bfddebedc608b8e282776a8d35ab3f13f62487
qmafaza <qmafaza@student.uns.ac.id>
2025-04-26 16:06:42 +0700

    seeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'product_name' => fake()->name(),
+            'description' => fake()->paragraph(),
+            'stock' => rand(1,10),
+            'product_price',
+            'product_image',
+        ];
+    }
+}
