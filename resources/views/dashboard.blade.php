013682d5a4508b39730592dfd77a6c6650c5f405
Abqli484 <abidawaqli@gmail.com>
2025-04-27 08:41:41 +0700

    dashboard edit bentar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,13 +31,6 @@
                 <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
                     <div class="mb-4 flex items-center justify-between gap-4 md:mb-8">
                     <h2 class="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">Shop by category</h2>
-
-                    <a href="#" title="" class="flex items-center text-base font-medium text-primary-700 hover:underline dark:text-primary-500">
-                        See more categories
-                        <svg class="ms-1 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4" />
-                        </svg>
-                    </a>
                     </div>
 
                     <div class="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
