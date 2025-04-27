d9334204c240d91c28466e09780ac19d2a72f1c7
qmafaza <qmafaza@student.uns.ac.id>
2025-04-27 12:33:45 +0700

    add product view

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,6 +17,12 @@
                     </x-nav-link>
                 </div>
 
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('product.index')" :active="request()->routeIs('product.index')">
+                        {{ __('Product') }}
+                    </x-nav-link>
+                </div>
+
             </div>
 
             <!-- Settings Dropdown -->
@@ -122,6 +128,12 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-nav-link :href="route('product.index')" :active="request()->routeIs('product.index')">
+                {{ __('Product') }}
+            </x-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
