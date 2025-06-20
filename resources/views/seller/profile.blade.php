4e742487dced90b62bdf4bd51bc675f69b22b909
Muhammad Danish Dhiyaulhaq <danishdhiyaulhaq6@gmail.com>
2025-06-20 19:39:45 +0700

    button process

--- resources/views/seller/profile.blade.php
+++ resources/views/seller/profile.blade.php
@@ -198,6 +198,35 @@
               In transit
             </dd>
           </dl>
+          <button
+            type="button"
+            id="processButton"
+            onclick="handleProcessing(this)"
+            data-modal-target="accountInformationModal2"
+            data-modal-toggle="accountInformationModal2"
+            class="inline-flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 sm:w-auto"
+          >
+            <svg class="-ms-0.5 me-1.5 h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"></path>
+            </svg>
+            <span>Process</span>
+          </button>
+
+          <script>
+            function handleProcessing(button) {
+              // Ganti isi tombol jadi "Processed"
+              button.innerHTML = `
+                <svg class="-ms-0.5 me-1.5 h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                </svg>
+                <span>Processed</span>
+              `;
+              
+              // Opsional: menonaktifkan tombol setelah diklik
+              button.disabled = true;
+              button.classList.add('bg-green-600', 'hover:bg-green-700');
+            }
+          </script>
   
           {{-- <div class="w-full sm:flex sm:w-32 sm:items-center sm:justify-end sm:gap-4">
             <button
@@ -269,6 +298,35 @@
               Cancelled
             </dd>
           </dl>
+          <button
+            type="button"
+            id="processButton"
+            onclick="handleProcessing(this)"
+            data-modal-target="accountInformationModal2"
+            data-modal-toggle="accountInformationModal2"
+            class="inline-flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 sm:w-auto"
+          >
+            <svg class="-ms-0.5 me-1.5 h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"></path>
+            </svg>
+            <span>Process</span>
+          </button>
+
+          <script>
+            function handleProcessing(button) {
+              // Ganti isi tombol jadi "Processed"
+              button.innerHTML = `
+                <svg class="-ms-0.5 me-1.5 h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                </svg>
+                <span>Processed</span>
+              `;
+              
+              // Opsional: menonaktifkan tombol setelah diklik
+              button.disabled = true;
+              button.classList.add('bg-green-600', 'hover:bg-green-700');
+            }
+          </script>
   
           {{-- <div class="w-full sm:flex sm:w-32 sm:items-center sm:justify-end sm:gap-4">
             <button
@@ -332,6 +390,35 @@
               Completed
             </dd>
           </dl>
+          <button
+            type="button"
+            id="processButton"
+            onclick="handleProcessing(this)"
+            data-modal-target="accountInformationModal2"
+            data-modal-toggle="accountInformationModal2"
+            class="inline-flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 sm:w-auto"
+          >
+            <svg class="-ms-0.5 me-1.5 h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"></path>
+            </svg>
+            <span>Process</span>
+          </button>
+
+          <script>
+            function handleProcessing(button) {
+              // Ganti isi tombol jadi "Processed"
+              button.innerHTML = `
+                <svg class="-ms-0.5 me-1.5 h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                </svg>
+                <span>Processed</span>
+              `;
+              
+              // Opsional: menonaktifkan tombol setelah diklik
+              button.disabled = true;
+              button.classList.add('bg-green-600', 'hover:bg-green-700');
+            }
+          </script>
   
           {{-- <div class="w-full sm:flex sm:w-32 sm:items-center sm:justify-end sm:gap-4">
             <button
@@ -395,6 +482,36 @@
               Completed
             </dd>
           </dl>
+          <button
+            type="button"
+            id="processButton"
+            onclick="handleProcessing(this)"
+            data-modal-target="accountInformationModal2"
+            data-modal-toggle="accountInformationModal2"
+            class="inline-flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 sm:w-auto"
+          >
+            <svg class="-ms-0.5 me-1.5 h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"></path>
+            </svg>
+            <span>Process</span>
+          </button>
+
+          <script>
+            function handleProcessing(button) {
+              // Ganti isi tombol jadi "Processed"
+              button.innerHTML = `
+                <svg class="-ms-0.5 me-1.5 h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                </svg>
+                <span>Processed</span>
+              `;
+              
+              // Opsional: menonaktifkan tombol setelah diklik
+              button.disabled = true;
+              button.classList.add('bg-green-600', 'hover:bg-green-700');
+            }
+          </script>
+
   
           {{-- <div class="w-full sm:flex sm:w-32 sm:items-center sm:justify-end sm:gap-4">
             <button
