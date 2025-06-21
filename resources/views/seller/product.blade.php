ed4e3cb5fdfc1d006c77f7798a21dbcee3d2a480
Muhammad Danish Dhiyaulhaq <danishdhiyaulhaq6@gmail.com>
2025-06-21 19:30:00 +0700

    ini yanto

--- resources/views/seller/product.blade.php
+++ resources/views/seller/product.blade.php
@@ -350,12 +350,12 @@
                                     </div>
                                 </div>
                             </div>
-                            
+
                         </div>
                     </form>     
                     </div>
                 </div>
-                <div class="overflow-x-auto">
+                <div class="overflow-x-auto min-h-[400px]">
                     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
