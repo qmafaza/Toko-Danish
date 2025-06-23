b47badcb9d441b8d3bf224fc292c09f59ca5ae54
qmafaza <qmafaza@student.uns.ac.id>
2025-06-23 20:28:06 +0700

    uwuwuw

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,8 +20,8 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->validateCsrfTokens(except: [
-            '/login',
-            '/register',
+            // '/login',
+            // '/register',
         ]);
 
     //     $middleware->trustProxies(at: [
